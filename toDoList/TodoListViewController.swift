79f67ae231cc2790defaa8ffd2edf4bdf70d28aa
Aleksandr Nikitin <alnikitin3@gmail.com>
2018-08-23 12:29:21 +0500

    уже удалил класс Items, до этого работал сейф в папку

--- toDoList/TodoListViewController.swift
+++ toDoList/TodoListViewController.swift
@@ -10,47 +10,51 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    //массив с записями, для их передачи в списки
+    var itemArray = [Items]()
     
-    var itemArray = ["Find Mike", "Buy Eggs", "Destroy World"]
-    let defaults = UserDefaults.standard
+    // Создали путь для сохранения всей информации
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        loadItems()
+
+//        let newItem = Items()
+//        newItem.title = "Find Miky"
+//        itemArray.append(newItem)
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
     }
 
     //MARK - TableView Data Source Methods
+    //количество строк
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    //Создаем ячейку (cell)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListCellIdentifier", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
-
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
         return cell
     }
 
     
     //MARK - TableView Delegate Methods
-
+    //Реакция на клик по ячейке (выбор ячейки)
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
-        
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        // снимаем/ставим галочку о выполнении
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // чисто для красоты, что бы не висело серым
         tableView.deselectRow(at: indexPath, animated: true)
-
+        // сохраняем в данные
+        saveItems()
+        
     }
     
     @IBAction func addRow(_ sender: Any) {
@@ -59,9 +63,11 @@ class TodoListViewController: UITableViewController {
         
         let allert = UIAlertController(title: "Add todoList", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (act) in
-            self.itemArray.append(textF.text!)
-            self.tableView.reloadData()
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            let newItem = Items()
+            newItem.title = textF.text!
+            self.itemArray.append(newItem)
+            self.saveItems()
+          //  self.defaults.set(self.itemArray, forKey: "TodoListArray")
         }
         
         allert.addAction(action)
@@ -74,7 +80,35 @@ class TodoListViewController: UITableViewController {
     }
     
     
+    func saveItems() {
+        
+        //переводит данные в plist
+        let encoder = PropertyListEncoder()
+        do {
+            // переводим данные в plist
+            let data = try encoder.encode(itemArray)
+            // пробуем их записать
+            try data.write(to: dataFilePath!)
+        } catch {
+            // Если не получилось выводим ошибку
+            print ("\(error)")
+        }
+        //перезагружаем таблицу
+        self.tableView.reloadData()
+    }
     
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+          do {
+            itemArray = try decoder.decode([Items].self, from: data)
+                //  itemArray = try decoder.decode([Items].self, from: data)
+            } catch {
+                print(error)
+           
+            }
+        }
+    }
     
 }
 
